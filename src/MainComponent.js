036d0ed5f16141dab770adbdd14baa331ae00329
Iman Dezfuly <Iman_Dezfuly@racv.com.au>
2020-06-17 19:02:14 +1000

    working project

--- src/MainComponent.js
+++ src/MainComponent.js
@@ -1,10 +1,66 @@
-import React from 'react';
-import { ExpansionPanel } from '@material-ui/core';
+import React, { useState } from 'react';
+import { ExpansionPanel, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Drawer from '@material-ui/core/Drawer';
+
+const ItemDisplay = ({ item }) => (
+	<div
+		style={{
+			padding: '10px',
+		}}>
+		<div> {item.name} </div>
+		{item &&
+			item.variants &&
+			item.variants.map(variant => (
+				<div
+					style={{
+						padding: '10px',
+						minWidth: '360px',
+						marginBottom: '40px',
+						marginTop: '20px',
+						border: 'solid 1px pink',
+						borderRadius: '10px',
+					}}>
+					<div> {variant.sku} </div>
+					<div>In Stock: {`${variant.inStock}`}</div>
+					<img
+						src={`https://www.aesop.com/au${variant.thumbnail}`}
+						style={{ width: '240px', height: '240px' }}
+					/>
+					<div>{variant.price}</div>
+				</div>
+			))}
+	</div>
+);
+
+const ItemComponent = ({ item }) => {
+	const [toggle, setToggle] = useState(false);
+
+	return (
+		<div>
+			<Button
+				onClick={() => {
+					item.drawerIsOpen = true;
+					setToggle(!toggle);
+				}}>
+				{item.name}
+			</Button>
+			<Drawer
+				anchor="left"
+				open={!!item.drawerIsOpen}
+				onClose={() => {
+					item.drawerIsOpen = false;
+					setToggle(!toggle);
+				}}>
+				<ItemDisplay item={item} />
+			</Drawer>
+		</div>
+	);
+};
 
 const useStyles = makeStyles(theme => ({
 	root: {
@@ -43,18 +99,9 @@ export const MainComponent = ({ data }) => {
 											{subCat.name}
 										</Typography>
 									</ExpansionPanelSummary>
-									<div style={{ paddingLeft: '30px' }}>
+									<div style={{ paddingLeft: '60px' }}>
 										{subCat.items.map(item => (
-											<ExpansionPanel key={item.name}>
-												<ExpansionPanelSummary
-													key={item.name}
-													aria-controls="panel1a-content"
-													id="panel1a-header">
-													<Typography className={classes.heading}>
-														{item.name}
-													</Typography>
-												</ExpansionPanelSummary>
-											</ExpansionPanel>
+											<ItemComponent key={item.name} item={item} />
 										))}
 									</div>
 								</ExpansionPanel>
